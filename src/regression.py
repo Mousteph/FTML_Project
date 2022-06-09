15f871c983fb3e34af19e8ef83c5ccd33c4dcf72
Moustapha DIOP <moustapha.diop@epita.fr>
2022-06-09 21:17:10 +0200

    (update) regression

--- src/regression.py
+++ src/regression.py
@@ -1,102 +1,82 @@
 import numpy as np
 
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression, RidgeCV, ElasticNetCV
+from sklearn.linear_model import LinearRegression, Ridge, ElasticNet
 from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 from sklearn.metrics import r2_score
 from sklearn.svm import SVR
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
 
 X = np.load("data/regression/inputs.npy")
 y = np.load("data/regression/labels.npy")
 
 print(f"Shape of X: {X.shape}")
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=20)
-scaler = StandardScaler().fit(X_train)
-X_train = scaler.transform(X_train)
-X_test = scaler.transform(X_test)
 
+class FitModelRegression:
+    def __init__(self, X, y):
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            X, y, random_state=20)
 
-def linear_regression(X_train, X_test, y_train, y_test):
-    model = LinearRegression().fit(X_train, y_train)
+        self.pipeline = Pipeline([('scaler', StandardScaler())])
+        self.pipeline.fit(self.X_train, self.y_train)
 
-    y_train_pred = model.predict(X_train)
-    y_test_pred = model.predict(X_test)
+        self.X_train = self.pipeline.transform(self.X_train)
+        self.X_test = self.pipeline.transform(self.X_test)
 
-    print(
-        f"Linear Regression R2 Score: "
-        f"Train = {r2_score(y_train, y_train_pred)} | "
-        f"Test = {r2_score(y_test, y_test_pred)}"
-    )
+    def find_best_model(self, model, params, random=False):
+        if not random:
+            grid_search = GridSearchCV(
+                model, params, cv=5, scoring='r2', return_train_score=True, n_jobs=-1)
+            grid_search.fit(self.X_train, self.y_train.flatten())
+            return grid_search
 
+        rnd_search = RandomizedSearchCV(
+            model, param_distributions=params, n_iter=200, cv=5, scoring='r2', random_state=20)
+        rnd_search.fit(self.X_train, self.y_train.flatten())
 
-def ridge(X_train, X_test, y_train, y_test):
-    alphas = np.arange(0.01, 15, 0.01)
+        return rnd_search
 
-    model = RidgeCV(alphas=alphas).fit(X_train, y_train)
-    print("Ridge best alpha:", model.alpha_)
+    def fit_new_model(self, model, params, name, random=False):
+        best_model = self.find_best_model(model, params, random)
+        print(f"{name} best model: {best_model.best_params_}")
 
-    y_train_pred = model.predict(X_train)
-    y_test_pred = model.predict(X_test)
+        y_train_pred = best_model.predict(self.X_train)
+        y_test_pred = best_model.predict(self.X_test)
 
-    print(
-        f"Ridge R2 Score: "
-        f"Train = {r2_score(y_train, y_train_pred)} | "
-        f"Test = {r2_score(y_test, y_test_pred)}"
-    )
+        print(f"{name} R2 Score: Train = {r2_score(self.y_train, y_train_pred)} | "
+              f"Test = {r2_score(self.y_test, y_test_pred)}")
 
-
-def support_vector_regression(X_train, X_test, y_train, y_test):
-    alphas = np.arange(0.01, 2, 0.01)
-    kernels = np.array(["linear", "poly", "rbf", "sigmoid"])
-
-    best_score = 0
-    best_model = None
-    best_parameters = {"kernel": None, "alpha": None}
-
-    for kernel in kernels:
-        for alpha in alphas:
-            model = SVR(kernel=kernel, C=alpha).fit(X_train, y_train.flatten())
-            score = model.score(X_test, y_test)
-            if score > best_score:
-                best_score = score
-                best_model = model
-                best_parameters["kernel"] = kernel
-                best_parameters["alpha"] = alpha
-
-    model = best_model
-    print("SVR best kernel:", best_parameters['kernel'])
-    print("SVR best alpha:", best_parameters['alpha'])
-
-    y_train_pred = model.predict(X_train)
-    y_test_pred = model.predict(X_test)
-
-    print(
-        f"SVR R2 Score: "
-        f"Train = {r2_score(y_train, y_train_pred)} | "
-        f"Test = {r2_score(y_test, y_test_pred)}"
-    )
-
-
-def elasic_net(X_train, X_test, y_train, y_test):
-    l1_ratio = np.arange(0.01, 1.01, 0.01)
-    model = ElasticNetCV(l1_ratio=l1_ratio,
-                         max_iter=4000).fit(X_train, y_train.flatten())
-
-    print("Elastic Net best l1_ratio:", model.l1_ratio_)
-
-    y_train_pred = model.predict(X_train)
-    y_test_pred = model.predict(X_test)
-
-    print(
-        f"Elastic Net R2 Score: "
-        f"Train = {r2_score(y_train, y_train_pred)} | "
-        f"Test = {r2_score(y_test, y_test_pred)}"
-    )
+        return Pipeline([('pipeline', self.pipeline), (name, best_model.best_estimator_)])
 
 
 if __name__ == '__main__':
-    linear_regression(X_train, X_test, y_train, y_test)
-    ridge(X_train, X_test, y_train, y_test)
-    support_vector_regression(X_train, X_test, y_train, y_test)
-    elasic_net(X_train, X_test, y_train, y_test)
+    model_regression = FitModelRegression(X, y)
+
+    lr_pipeline = model_regression.fit_new_model(LinearRegression(), dict(),
+                                                 "Linear Regression")
+
+    params_rigde = [
+        {"alpha": np.arange(0.01, 5, 0.01),
+         "solver": ["svd", "cholesky", "lsqr", "sparse_cg", "sag", "saga"]}
+    ]
+    ridge_pipeline = model_regression.fit_new_model(Ridge(random_state=20),
+                                                    params_rigde, "Ridge")
+
+    params_SVR = [
+        {"C": np.arange(0.01, 2, 0.02),
+         "kernel": ["linear", "rbf", "sigmoid"],
+         "epsilon": np.arange(0.1, 1, 0.1)}
+    ]
+    svr_pipeline = model_regression.fit_new_model(SVR(), params_SVR, "SVR",
+                                                  random=True)
+
+    params_en = [
+        {"alpha": np.arange(0.01, 2, 0.02),
+         "l1_ratio": np.arange(0.01, 1.01, 0.01)}
+    ]
+
+    en_pipeline = model_regression.fit_new_model(ElasticNet(random_state=20,
+                                                            max_iter=10000),
+                                                 params_en, "ElasticNet")
